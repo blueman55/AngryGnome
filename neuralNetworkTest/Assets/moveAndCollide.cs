e562d02353044fa1297d4091292fc1a6e2f393aa
Janelle Flaherty <flahertyj6@wit.edu>
2015-07-16 18:52:15 -0400

    added neural network project

---
+++ neuralNetworkTest/Assets/moveAndCollide.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveAndCollide : MonoBehaviour {
+	bool hit = false;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (hit == false) {
+		print ("I'm alive");
+		} else {
+			print("colliding with object Master");
+		}
+		transform.localPosition -= transform.forward * Time.deltaTime*3f;
+	}
+
+	void OnCollisionEnter(Collision coll)
+	{
+
+		if(coll.gameObject.name=="testWall")
+			hit = true;
+			print ("colliding with wall master!");    
+	}
+}
