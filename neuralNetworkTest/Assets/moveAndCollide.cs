fe70f953d4f621f07aed61163a4fe678cea0f809
Janelle Flaherty <flahertyj6@wit.edu>
2015-07-17 01:42:43 -0400

    added basics for collision and sensors

--- neuralNetworkTest/Assets/moveAndCollide.cs
+++ neuralNetworkTest/Assets/moveAndCollide.cs
@@ -2,27 +2,30 @@
 using System.Collections;
 
 public class moveAndCollide : MonoBehaviour {
+	//var sensorPrefab:Transform;
 	bool hit = false;
 	// Use this for initialization
 	void Start () {
-
+		GameObject sensor = (GameObject)Instantiate(Resources.Load("sensorPrefab"), GameObject.Find("spawnPoint").transform.position, Quaternion.identity); //change myprefab to name
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (hit == false) {
-		print ("I'm alive");
-		} else {
-			print("colliding with object Master");
-		}
-		transform.localPosition -= transform.forward * Time.deltaTime*3f;
+
+		//movement for units
+		//transform.localPosition -= transform.forward * Time.deltaTime*3f;
+		// end movement
+
+		//sensor stuff
+		// HERE GOES time controls on how often fires
+		//GameObject sensor = (GameObject)Instantiate(Resources.Load("sensorPrefab"), GameObject.Find("spawnPoint").transform.position, Quaternion.identity); //change myprefab to name
+		//
 	}
 
 	void OnCollisionEnter(Collision coll)
 	{
 
-		if(coll.gameObject.name=="testWall")
-			hit = true;
-			print ("colliding with wall master!");    
-	}
+		//if(coll.gameObject.name=="testWall")    
+	//}
+}
 }
---
+++ neuralNetworkTest/Assets/sensorScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sensorScript : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		//while time
+		transform.Translate (0.05f,0,0);
+		Destroy (gameObject, 1f);
+
+	}
+	void OnCollisionEnter(Collision coll)
+	{
+		
+		//if(coll.gameObject.name=="testWall")    
+		//}
+
+		//do other stuff
+	}
+}
