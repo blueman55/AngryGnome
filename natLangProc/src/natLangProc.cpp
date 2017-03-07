34875ae63d515c3651b2258c98273ba52fb0f7d6
Janelle Flaherty <flahertyj6@wit.edu>
2017-03-06 22:30:51 -0500

    06-03-2017 jf project setup

---
+++ natLangProc/src/natLangProc.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : natLangProc.cpp
+// Author      : Janelle Flaherty, James Flaherty
+// Version     :
+// Copyright   : Please.. Don't steal?
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "uh. hello?" << endl; // prints uh. hello?
+	return 0;
+}
